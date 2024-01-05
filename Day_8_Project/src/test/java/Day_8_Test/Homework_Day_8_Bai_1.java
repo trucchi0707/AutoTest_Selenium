f75b0d0df8f4726a6ac72fcf212567f86ab7beea
trucchi0707 <48147103+trucchi0707@users.noreply.github.com>
2024-01-05 17:31:27 +0700

    First Comnmit

---
+++ Day_8_Project/src/test/java/Day_8_Test/Homework_Day_8_Bai_1.java
@@ -0,0 +1,81 @@
+package Day_8_Test;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+
+public class Homework_Day_8_Bai_1 {
+	
+	WebDriver driver;
+	
+  @Test
+  public void Testcase_1() {
+	  WebElement imgClose = driver.findElement(By.className("sgpb-popup-close-button-6"));
+	  imgClose.click();
+	  try {
+		  Thread.sleep(2000);
+	  } 
+	  catch(InterruptedException e) {
+		  e.printStackTrace();
+	  }
+	  
+	  // Có lỗi ở field Email => Bỏ qua field này sẽ run được script
+	  
+//	  WebElement txtEmail = driver.findElement(By.name("email"));
+////	  WebElement txtEmail = driver.findElement(By.xpath("//input[@title='Email']"));
+////	  WebElement txtEmail = driver.findElement(By.xpath("//input[@autocomplete='anyrandomstring']"));
+//	  
+//	  txtEmail.clear();
+//	  txtEmail.sendKeys("testabc@gmail.com");
+	  
+	  WebElement txtPassword = driver.findElement(By.id("pass"));
+	  txtPassword.clear();
+	  txtPassword.sendKeys("123abc");
+	  
+	  WebElement txtCompany = driver.findElement(By.name("company"));
+	  txtCompany.clear();
+	  txtCompany.sendKeys("Company ABC");
+	  
+	  WebElement txtMobileNumber = driver.findElement(By.name("mobile number"));
+	  txtMobileNumber.clear();
+	  txtMobileNumber.sendKeys("0123456789");
+	  
+	  try {
+		  Thread.sleep(2000);
+	  } 
+	  catch(InterruptedException e) {
+		  e.printStackTrace();
+	  }
+	  
+	  WebElement btnSubmit = driver.findElement(By.xpath("//input[@type='submit']"));
+	  btnSubmit.click();
+	  
+	 
+  }
+  @BeforeClass
+  public void beforeClass() {
+	  driver = new FirefoxDriver();
+	  //driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.get("https://selectorshub.com/xpath-practice-page/");
+	  
+	  try {
+		  Thread.sleep(5000);
+	  } 
+	  catch(InterruptedException e) {
+		  e.printStackTrace();
+	  }
+	  
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+
+}
---
+++ Day_8_Project/src/test/java/Day_8_Test/Homework_Day_8_Bai_2.java
@@ -0,0 +1,123 @@
+package Day_8_Test;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.openqa.selenium.JavascriptExecutor;
+public class Homework_Day_8_Bai_2 {
+	
+	WebDriver driver;
+	
+  @Test
+  public void Testcase_1 () {
+	  WebElement scrollpage = driver.findElement(By.id("login-form"));
+	  ((JavascriptExecutor)driver).executeScript ("arguments[0].scrollIntoView();", scrollpage);
+	  try {
+		  Thread.sleep(2000);
+	  } 
+	  catch(InterruptedException e) {
+		  e.printStackTrace();
+	  }
+
+	  WebElement txtName = driver.findElement(By.id("name"));
+	  txtName.clear();
+	  txtName.sendKeys("Test Submit");
+	  
+	  WebElement txtAddress = driver.findElement(By.id("address"));
+	  txtAddress.clear();
+	  txtAddress.sendKeys("address ABC Submit");
+	  
+	  WebElement txtEmail = driver.findElement(By.id("email"));
+	  txtEmail.clear();
+	  txtEmail.sendKeys("testsubmit@gmail.com");
+	  
+	  WebElement txtPassword = driver.findElement(By.id("password"));
+	  txtPassword.clear();
+	  txtPassword.sendKeys("Test123");
+	  
+	  try {
+		  Thread.sleep(3000);
+	  } 
+	  catch(InterruptedException e) {
+		  e.printStackTrace();
+	  }
+	  
+	  WebElement btnSubmit = driver.findElement(By.xpath("//button[@value='Submit']"));
+	  btnSubmit.click();	  
+	  
+	  try {
+		  Thread.sleep(2000);
+	  } 
+	  catch(InterruptedException e) {
+		  e.printStackTrace();
+	  }
+  }
+  
+  @Test
+  public void Testcase_2 () {
+	  WebElement scrollpage = driver.findElement(By.id("login-form"));
+	  ((JavascriptExecutor)driver).executeScript ("arguments[0].scrollIntoView();", scrollpage);
+	  try {
+		  Thread.sleep(2000);
+	  } 
+	  catch(InterruptedException e) {
+		  e.printStackTrace();
+	  }
+
+	  WebElement txtName = driver.findElement(By.id("name"));
+	  txtName.clear();
+	  txtName.sendKeys("TestReset");
+	  
+	  WebElement txtAddress = driver.findElement(By.id("address"));
+	  txtAddress.clear();
+	  txtAddress.sendKeys("address ABC Reset");
+	  
+	  WebElement txtEmail = driver.findElement(By.id("email"));
+	  txtEmail.clear();
+	  txtEmail.sendKeys("testreset@gmail.com");
+	  
+	  WebElement txtPassword = driver.findElement(By.id("password"));
+	  txtPassword.clear();
+	  txtPassword.sendKeys("Test123");
+	  
+	  try {
+		  Thread.sleep(3000);
+	  } 
+	  catch(InterruptedException e) {
+		  e.printStackTrace();
+	  }
+	  
+	  WebElement btnReset = driver.findElement(By.xpath("//button[@value='Reset']"));
+	  btnReset.click();
+	  try {
+		  Thread.sleep(2000);
+	  } 
+	  catch(InterruptedException e) {
+		  e.printStackTrace();
+	  }
+	  
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.get("https://automationfc.github.io/basic-form/index.html");
+	  try {
+		  Thread.sleep(2000);
+	  }
+	  catch(InterruptedException e) {
+		  e.printStackTrace();
+	  }
+	  
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
